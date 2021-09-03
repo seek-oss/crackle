c94a79a05211ce3f743f4925b68063584e952a88
Ben Jervis <bjervis@seek.com.au>
2021-09-03 10:10:40 +1000

    Add typechecking

---
+++ fixtures/web-app/src/braid.d.ts
@@ -0,0 +1,9 @@
+declare module 'sku/treat' {
+  export const createTheme = (_input: any) => '';
+  export type Style = any;
+}
+
+declare module 'sku/react-treat' {
+  export const TreatProvider = (_props: { children: any; theme: any }) => any;
+  export const useTheme = () => '';
+}
