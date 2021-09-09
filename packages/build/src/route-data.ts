7f1f8b5b0ed848b8259d3deaf8adc6fc5a1bb529
Ben Jervis <bjervis@seek.com.au>
2021-09-09 15:35:00 +1000

    Finish the routes script

--- packages/build/src/route-data.ts
+++ packages/build/src/route-data.ts
@@ -1,8 +1,30 @@
+import { transformFileAsync as babelTransform } from '@babel/core';
+import removeExportsPlugin from '@crackle/babel-plugin-remove-exports';
 import { build as esbuild } from 'esbuild';
 import glob from 'fast-glob';
 
 const routesEntryName = 'ROUTES_ENTRY';
 const routeEntryNs = 'ROUTES_ENTRY_NAMESPACE';
+
+const transformWithBabel = async (path: string) => {
+  const transformedContents = await babelTransform(path, {
+    plugins: [
+      removeExportsPlugin,
+      '@babel/plugin-syntax-jsx',
+      ['@babel/plugin-syntax-typescript', { isTSX: true }],
+    ],
+    babelrc: false,
+  });
+
+  if (!transformedContents?.code) {
+    throw new Error(`No result from babel plugin transform of ${path}`);
+  }
+
+  return {
+    contents: transformedContents.code,
+    loader: 'ts',
+  } as const;
+};
 
 export const getAllRoutes = async () => {
   const root = process.cwd();
@@ -12,7 +34,7 @@ export const getAllRoutes = async () => {
   const pageImports = pageFiles
     .map(
       (pageFile, index) =>
-        `import { routeData as page${index} } from './${pageFile}'; pages.push(page${index}());`,
+        `import { routeData as page${index} } from './${pageFile}'; pages.push({ path: '${pageFile}', ...page${index}() });`,
     )
     .join('\n');
 
@@ -23,7 +45,6 @@ export const getAllRoutes = async () => {
     outdir: 'out',
     target: ['node14'],
     format: 'cjs',
-    external: ['braid-design-system'],
     plugins: [
       {
         name: 'routes-virtual-entry',
@@ -34,13 +55,20 @@ export const getAllRoutes = async () => {
             namespace: routeEntryNs,
           }));
 
+          build.onLoad({ filter: new RegExp('.*.page.tsx$') }, ({ path }) =>
+            transformWithBabel(path),
+          );
+
+          build.onLoad(
+            { filter: new RegExp('.*src/pages/.*.tsx$') },
+            ({ path }) => transformWithBabel(path),
+          );
+
           build.onLoad({ filter, namespace: routeEntryNs }, () => ({
             contents: `
-              const pages = [];
-              ${pageImports}
-
-              console.log('pages', pages);
-            `,
+            const pages = [];
+            ${pageImports}
+            exports.pages = pages;`,
             resolveDir: root,
           }));
         },
@@ -50,7 +78,6 @@ export const getAllRoutes = async () => {
 
   const [{ text: routesSource }] = result.outputFiles;
 
-  console.log('routesSource', routesSource);
-
-  eval(routesSource);
+  // eslint-disable-next-line no-eval
+  return eval(routesSource);
 };
