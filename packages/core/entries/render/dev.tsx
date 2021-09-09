cde2bc47bb4d2010f598a15a77ec269c7ee3754d
Ben Jervis <bjervis@seek.com.au>
2021-09-09 17:22:42 +1000

    Add basic page

--- packages/core/entries/render/dev.tsx
+++ packages/core/entries/render/dev.tsx
@@ -7,6 +7,7 @@ import { renderToString } from 'react-dom/server';
 
 import type { RenderDevPageFn } from '../types';
 
+import { NotFoundPage } from './NotFoundPage';
 import { Page, createRouteMap, nodePageModules } from './shared';
 
 const reactRefresh = `
@@ -25,37 +26,39 @@ export const renderDevelopmentPage: RenderDevPageFn = async ({
 }) => {
   const routeMap = createRouteMap();
 
+  let html = '';
+
   if (!routeMap[path]) {
-    throw new Error(`Unable to find path ${path}`);
+    html = renderToString(
+      <NotFoundPage attemptedPath={path} routeMap={routeMap} />,
+    );
+  } else {
+    const { default: PageComponent } = nodePageModules[routeMap[path].pageName];
+
+    const pageData = { routeMap };
+
+    const bodyTags = [
+      <script
+        key="react-refresh"
+        type="module"
+        dangerouslySetInnerHTML={{ __html: reactRefresh }}
+      />,
+      <script key="vite-client" type="module" src="/@vite/client" />,
+      <script key="entry" type="module" src={entry} />,
+    ];
+
+    html = renderToString(
+      <Page
+        path={path}
+        headTags={null}
+        bodyTags={bodyTags}
+        pageData={pageData}
+        criticalCssPlaceholder={criticalCssPlaceholder}
+      >
+        <PageComponent />
+      </Page>,
+    );
   }
-
-  const { default: PageComponent } = nodePageModules[routeMap[path].pageName];
-
-  const pageData = {
-    routeMap,
-  };
-
-  const bodyTags = [
-    <script
-      key="react-refresh"
-      type="module"
-      dangerouslySetInnerHTML={{ __html: reactRefresh }}
-    />,
-    <script key="vite-client" type="module" src="/@vite/client" />,
-    <script key="entry" type="module" src={entry} />,
-  ];
-
-  const html = renderToString(
-    <Page
-      path={path}
-      headTags={null}
-      bodyTags={bodyTags}
-      pageData={pageData}
-      criticalCssPlaceholder={criticalCssPlaceholder}
-    >
-      <PageComponent />
-    </Page>,
-  );
 
   return {
     html: await inlineCriticalCss(html, criticalCssPlaceholder),
