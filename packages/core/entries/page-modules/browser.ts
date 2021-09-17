c198805c93a9fc79c6b96742dc6f323607759788
Ben Jervis <bjervis@seek.com.au>
2021-09-17 22:25:54 +1000

    Handle single page roots and leading/trailing slashes

--- packages/core/entries/page-modules/browser.ts
+++ packages/core/entries/page-modules/browser.ts
@@ -2,9 +2,7 @@ import type { PageModule } from '../types';
 
 type PageModules = Record<string, () => Promise<PageModule>>;
 // @ts-expect-error
-const remotePageModules: PageModules = import.meta.glob(
-  `/{__PAGE_ROOTS}/**/*.page.tsx`,
-);
+const remotePageModules: PageModules = import.meta.glob('/__PAGE_ROOTS');
 
 export const browserPageModules: PageModules = {
   ...remotePageModules,
