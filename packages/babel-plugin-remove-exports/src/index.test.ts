97f421edf3874d84f20145010a26c178b9c4f214
Remus Mate <rmate@seek.com.au>
2022-08-12 11:02:57 +1000

    Add vitest

--- packages/babel-plugin-remove-exports/src/index.test.ts
+++ packages/babel-plugin-remove-exports/src/index.test.ts
@@ -2,234 +2,243 @@ import pluginTester from 'babel-plugin-tester/pure';
 
 import plugin from './';
 
+const ts = String.raw;
+
 const tests = [
   {
     title: 'Used import',
     pluginOptions: {
       retainExports: ['routeData'],
     },
-    code: `
-        import { createRouteData } from '@crackle/router';
-        import React from 'react';
+    code: ts/* ts */ `
+            import { createRouteData } from "@crackle/router";
+            import React from "react";
 
-        import { routeSetterUpper, unusedFunctions } from './internalRoutingLogic';
+            import { routeSetterUpper, unusedFunctions } from "./internalRoutingLogic";
 
-        import { Something } from '../Somewhere';
+            import { Something } from "../Somewhere";
 
-        export const routeData = createRouteData<BraidMetadata>(() => {
-          const myRoute = '/details';
-          return {
-            route: myRoute,
-            someDetail: routeSetterUpper(),
-            globalMetadata: {
-              isDeprecated: true,
-              componentName: 'Details',
-            },
-          }
-        });
+            export const routeData = createRouteData<BraidMetadata>(() => {
+              const myRoute = "/details";
+              return {
+                route: myRoute,
+                someDetail: routeSetterUpper(),
+                globalMetadata: {
+                  isDeprecated: true,
+                  componentName: "Details",
+                },
+              };
+            });
 
-        export default function () {
-          return <Something />;
-        }`,
+            export default function () {
+              return <Something />;
+            }
+          `,
   },
   {
     title: 'Braid import',
     pluginOptions: {
       retainExports: ['routeData'],
     },
-    code: `
-        import { createRouteData } from '@crackle/router';
-        import { Card, Stack } from 'braid-design-system';
-        import React from 'react';
+    code: ts/* ts */ `
+            import { createRouteData } from "@crackle/router";
+            import { Card, Stack } from "braid-design-system";
+            import React from "react";
 
-        import { routeSetterUpper, unusedFunctions } from './internalRoutingLogic';
+            import { routeSetterUpper, unusedFunctions } from "./internalRoutingLogic";
 
-        import { Something } from '../Somewhere';
+            import { Something } from "../Somewhere";
 
-        export const routeData = createRouteData<BraidMetadata>(() => {
-          const myRoute = '/details';
-          return {
-            route: myRoute,
-            someDetail: routeSetterUpper(),
-            globalMetadata: {
-              isDeprecated: true,
-              componentName: 'Details',
-            },
-          }
-        });
+            export const routeData = createRouteData<BraidMetadata>(() => {
+              const myRoute = "/details";
+              return {
+                route: myRoute,
+                someDetail: routeSetterUpper(),
+                globalMetadata: {
+                  isDeprecated: true,
+                  componentName: "Details",
+                },
+              };
+            });
 
-        export default function () {
-          return (
-            <Card>
-              <Stack space="medium">
-                <Something />
-                <Something />
-              </Stack>
-            </Card>
-          )
-        }`,
+            export default function () {
+              return (
+                <Card>
+                  <Stack space="medium">
+                    <Something />
+                    <Something />
+                  </Stack>
+                </Card>
+              );
+            }
+          `,
   },
   {
     title: 'Used object',
     pluginOptions: {
       retainExports: ['routeData'],
     },
-    code: `
-        import { createRouteData } from '@crackle/router';
-        import React from 'react';
+    code: ts/* ts */ `
+            import { createRouteData } from "@crackle/router";
+            import React from "react";
 
-        const myMetadata = {
-          isDeprecated: true,
-          componentName: 'Details',
-        }
+            const myMetadata = {
+              isDeprecated: true,
+              componentName: "Details",
+            };
 
-        export const routeData = createRouteData<BraidMetadata>(() => ({
-          route: '/details',
-          globalMetadata: myMetadata,
-        }));
+            export const routeData = createRouteData<BraidMetadata>(() => ({
+              route: "/details",
+              globalMetadata: myMetadata,
+            }));
 
-        const MyComponent = () => <div />
+            const MyComponent = () => <div />;
 
-        export default function () {
-          return <MyComponent />;
-        }`,
+            export default function () {
+              return <MyComponent />;
+            }
+          `,
   },
   {
     title: 'Nested object',
     pluginOptions: {
       retainExports: ['routeData'],
     },
-    code: `
-        import { createRouteData } from '@crackle/router';
-        import React from 'react';
+    code: ts/* ts */ `
+            import { createRouteData } from "@crackle/router";
+            import React from "react";
 
-        const componentName = 'Details';
+            const componentName = "Details";
 
-        const myMetadata = {
-          isDeprecated: true,
-          componentName,
-        };
+            const myMetadata = {
+              isDeprecated: true,
+              componentName,
+            };
 
-        export const routeData = createRouteData<BraidMetadata>(() => ({
-          route: '/details',
-          globalMetadata: myMetadata,
-        }));
+            export const routeData = createRouteData<BraidMetadata>(() => ({
+              route: "/details",
+              globalMetadata: myMetadata,
+            }));
 
-        const MyComponent = () => <div />;
+            const MyComponent = () => <div />;
 
-        export default function () {
-          return <MyComponent />;
-        }`,
+            export default function () {
+              return <MyComponent />;
+            }
+          `,
   },
   {
     title: 'Super nested object',
     pluginOptions: {
       retainExports: ['routeData'],
     },
-    code: `
-        import { createRouteData } from '@crackle/router';
-        import React from 'react';
+    code: ts/* ts */ `
+            import { createRouteData } from "@crackle/router";
+            import React from "react";
 
-        const componentName = 'Details';
+            const componentName = "Details";
 
-        const myMetadata = {
-          isDeprecated: true,
-          componentName,
-        };
+            const myMetadata = {
+              isDeprecated: true,
+              componentName,
+            };
 
-        const routeDetails = {
-          route: '/details',
-          globalMetadata: myMetadata,
-        }
+            const routeDetails = {
+              route: "/details",
+              globalMetadata: myMetadata,
+            };
 
-        export const routeData = createRouteData<BraidMetadata>(() => routeDetails);
+            export const routeData = createRouteData<BraidMetadata>(() => routeDetails);
 
-        const MyComponent = () => <div />;
+            const MyComponent = () => <div />;
 
-        export default function () {
-          return <MyComponent />;
-        }`,
+            export default function () {
+              return <MyComponent />;
+            }
+          `,
   },
   {
     title: 'Separated named export',
     pluginOptions: {
       retainExports: ['routeData'],
     },
-    code: `
-        import { createRouteData } from '@crackle/router';
-        import React from 'react';
+    code: ts/* ts */ `
+            import { createRouteData } from "@crackle/router";
+            import React from "react";
 
-        const routeData = createRouteData<BraidMetadata>(() => ({
-          route: '/details',
-          globalMetadata: {
-            isDeprecated: true,
-            componentName: 'Details',
-          },
-        }));
+            const routeData = createRouteData<BraidMetadata>(() => ({
+              route: "/details",
+              globalMetadata: {
+                isDeprecated: true,
+                componentName: "Details",
+              },
+            }));
 
-        const MyComponent = () => <div />;
+            const MyComponent = () => <div />;
 
-        export default function () {
-          return <MyComponent />;
-        }
+            export default function () {
+              return <MyComponent />;
+            }
 
-        export { routeData };`,
+            export { routeData };
+          `,
   },
   {
     title: 'Renamed export',
     pluginOptions: {
       retainExports: ['routeData'],
     },
-    code: `
-        import { createRouteData } from '@crackle/router';
-        import React from 'react';
+    code: ts/* ts */ `
+            import { createRouteData } from "@crackle/router";
+            import React from "react";
 
-        const routes = createRouteData<BraidMetadata>(() => ({
-          route: '/details',
-          globalMetadata: {
-            isDeprecated: true,
-            componentName: 'Details',
-          },
-        }));
+            const routes = createRouteData<BraidMetadata>(() => ({
+              route: "/details",
+              globalMetadata: {
+                isDeprecated: true,
+                componentName: "Details",
+              },
+            }));
 
-        const MyComponent = () => <div />;
+            const MyComponent = () => <div />;
 
-        export default function () {
-          return <MyComponent />;
-        }
+            export default function () {
+              return <MyComponent />;
+            }
 
-        export { routes as routeData };`,
+            export { routes as routeData };
+          `,
   },
   {
     title: 'Local variable used in an imported function',
     pluginOptions: {
       retainExports: ['routeData'],
     },
-    code: `
-        import { createRouteData } from '@crackle/router';
-        import React from 'react';
+    code: ts/* ts */ `
+            import { createRouteData } from "@crackle/router";
+            import React from "react";
 
-        import { routeSetupFunc } from './settyThing';
+            import { routeSetupFunc } from "./settyThing";
 
-        const componentDetails = 'siteEn';
+            const componentDetails = "siteEn";
 
-        const routeData = createRouteData<BraidMetadata>(() => ({
-          route: '/details',
-          globalMetadata: {
-            detailsKey: routeSetupFunc(componentDetails),
-            isDeprecated: true,
-            componentName: 'Details',
-          },
-        }));
+            const routeData = createRouteData<BraidMetadata>(() => ({
+              route: "/details",
+              globalMetadata: {
+                detailsKey: routeSetupFunc(componentDetails),
+                isDeprecated: true,
+                componentName: "Details",
+              },
+            }));
 
-        const MyComponent = () => <div />;
+            const MyComponent = () => <div />;
 
-        export default function () {
-          return <MyComponent />;
-        }
+            export default function () {
+              return <MyComponent />;
+            }
 
-        export { routeData };
-  `,
+            export { routeData };
+          `,
   },
   {
     title: 'Keep default export, Local variable used in an imported function',
@@ -238,31 +247,31 @@ const tests = [
       retainDefault: true,
       retainIdentifiers: ['React'],
     },
-    code: `
-      import { createRouteData } from '@crackle/router';
-      import React from 'react';
+    code: ts/* ts */ `
+            import { createRouteData } from "@crackle/router";
+            import React from "react";
 
-      import { routeSetupFunc } from './settyThing';
+            import { routeSetupFunc } from "./settyThing";
 
-      const componentDetails = 'siteEn';
+            const componentDetails = "siteEn";
 
-      const routeData = createRouteData<BraidMetadata>(() => ({
-        route: '/details',
-        globalMetadata: {
-          detailsKey: routeSetupFunc(componentDetails),
-          isDeprecated: true,
-          componentName: 'Details',
-        },
-      }));
+            const routeData = createRouteData<BraidMetadata>(() => ({
+              route: "/details",
+              globalMetadata: {
+                detailsKey: routeSetupFunc(componentDetails),
+                isDeprecated: true,
+                componentName: "Details",
+              },
+            }));
 
-      const MyComponent = () => <div />;
+            const MyComponent = () => <div />;
 
-      export default function () {
-        return <MyComponent />;
-      }
+            export default function () {
+              return <MyComponent />;
+            }
 
-      export { routeData };
-`,
+            export { routeData };
+          `,
   },
 ];
 
--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -4,51 +4,51 @@ import { resolveConfig } from '@crackle/core/resolve-config';
 import { getAllRoutes } from '@crackle/core/route-data';
 
 test('getAllRoutes', async () => {
-  const cwd = path.join(__dirname, '../fixtures/monorepo');
+  const cwd = path.join(__dirname, '../fixtures/monorepo/site');
   const config = await resolveConfig({ cwd });
 
   expect(await getAllRoutes(config)).toMatchInlineSnapshot(`
-Array [
-  Object {
-    "globalMetadata": Object {
-      "componentName": "Details",
-      "isDeprecated": true,
-    },
-    "path": "site/src/pages/details.page.tsx",
-    "route": "/details",
-  },
-  Object {
-    "globalMetadata": Object {
-      "componentName": "Home",
-      "isDeprecated": false,
-    },
-    "path": "site/src/pages/home.page.tsx",
-    "route": "/",
-  },
-  Object {
-    "globalMetadata": Object {
-      "componentName": "Test",
-      "isDeprecated": false,
-    },
-    "path": "site/src/pages/testPage.page.tsx",
-    "route": "/test",
-  },
-  Object {
-    "globalMetadata": Object {
-      "componentName": "ExtraPage",
-      "isDeprecated": false,
-    },
-    "path": "site/src/components/RemotePage/ErrorPage.page.tsx",
-    "route": "/extra",
-  },
-  Object {
-    "globalMetadata": Object {
-      "componentName": "Remote page",
-      "isDeprecated": false,
-    },
-    "path": "site/src/components/RemotePage/RemotePage.page.tsx",
-    "route": "/remote/page",
-  },
-]
-`);
+    [
+      {
+        "globalMetadata": {
+          "componentName": "Details",
+          "isDeprecated": true,
+        },
+        "path": "src/pages/details.page.tsx",
+        "route": "/details",
+      },
+      {
+        "globalMetadata": {
+          "componentName": "Home",
+          "isDeprecated": false,
+        },
+        "path": "src/pages/home.page.tsx",
+        "route": "/",
+      },
+      {
+        "globalMetadata": {
+          "componentName": "Test",
+          "isDeprecated": false,
+        },
+        "path": "src/pages/testPage.page.tsx",
+        "route": "/test",
+      },
+      {
+        "globalMetadata": {
+          "componentName": "ExtraPage",
+          "isDeprecated": false,
+        },
+        "path": "src/components/RemotePage/ErrorPage.page.tsx",
+        "route": "/extra",
+      },
+      {
+        "globalMetadata": {
+          "componentName": "Remote page",
+          "isDeprecated": false,
+        },
+        "path": "src/components/RemotePage/RemotePage.page.tsx",
+        "route": "/remote/page",
+      },
+    ]
+  `);
 });
