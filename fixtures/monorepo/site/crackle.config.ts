1771eabf7984c502a10e23d412b2ef30902c062a
mattcompiles <mattjones701@gmail.com>
2022-09-08 14:48:21 +1000

    Migrate to pnpm and flatten monorepo fixture (#29)

--- fixtures/monorepo/site/crackle.config.ts
+++
@@ -1,8 +0,0 @@
-import type { CrackleConfig } from '@crackle/core';
-
-const config: CrackleConfig = {
-  pageRoots: ['src/pages', 'src/components/RemotePage'],
-  appShell: 'src/App.tsx',
-};
-
-export default config;
