64559c8b959b1eb0f09f7e909dc4b30696883ec2
Remus Mate <rmate@seek.com.au>
2022-09-19 15:47:18 +1000

    bundle each entry separately to avoid chunking

--- packages/core/src/package/dts.ts
+++ packages/core/src/package/dts.ts
@@ -5,29 +5,33 @@ import dts from 'rollup-plugin-dts';
 import type { EnhancedConfig } from '../config';
 import { externals } from '../plugins/rollup';
 import type { PackageEntryPoint } from '../types';
+import { promiseMap } from '../utils/promise-map';
 
 export const createDtsBundle = async (
   config: EnhancedConfig,
   entries: PackageEntryPoint[],
   outputOptions: OutputOptions,
 ) => {
-  const bundle = await rollup({
-    input: entries.map(({ entryPath }) => entryPath),
-    plugins: [
-      externals(config.root),
-      dts({
-        respectExternal: true,
-        compilerOptions: config.dtsOptions as any,
-      }),
-    ],
-  });
+  // We're bundling each entry separately to avoid chunking
+  await promiseMap(entries, async (entry) => {
+    const bundle = await rollup({
+      input: entry.entryPath,
+      plugins: [
+        externals(config.root),
+        dts({
+          respectExternal: true,
+          compilerOptions: config.dtsOptions as any,
+        }),
+      ],
+    });
 
-  await bundle.write({
-    ...outputOptions,
-    chunkFileNames: 'dist/[name]-[hash].d.ts',
-    exports: 'named',
-    format: 'esm',
-  });
+    await bundle.write({
+      ...outputOptions,
+      exports: 'named',
+      format: 'esm',
+      minifyInternalExports: false,
+    });
 
-  await bundle.close();
+    await bundle.close();
+  });
 };
