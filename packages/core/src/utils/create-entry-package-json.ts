22a83a11768505ec6427d7305d4f58459f0299bd
Remus Mate <rmate@seek.com.au>
2022-09-20 12:59:51 +1000

    fix generation of dev declaration files

--- packages/core/src/utils/create-entry-package-json.ts
+++ packages/core/src/utils/create-entry-package-json.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-import type { PackageEntryPoint } from '../types';
+import type { Format, PackageEntryPoint } from '../types';
 
 import { writePackageJson } from './files';
 import { promiseMap } from './promise-map';
@@ -8,19 +8,17 @@ import { promiseMap } from './promise-map';
 export const createEntryPackageJsons = async (
   entryPoints: PackageEntryPoint[],
 ) => {
+  const relativeOutputPath = (entryPoint: PackageEntryPoint, format: Format) =>
+    path.relative(entryPoint.outputDir, entryPoint.getOutputPath(format));
+
   await promiseMap(entryPoints, async (entryPoint) => {
     if (!entryPoint.isDefaultEntry) {
       await writePackageJson({
         dir: entryPoint.outputDir,
         contents: {
-          main: path.relative(
-            entryPoint.outputDir,
-            entryPoint.getOutputPath('cjs'),
-          ),
-          module: path.relative(
-            entryPoint.outputDir,
-            entryPoint.getOutputPath('esm'),
-          ),
+          main: relativeOutputPath(entryPoint, 'cjs'),
+          module: relativeOutputPath(entryPoint, 'esm'),
+          types: relativeOutputPath(entryPoint, 'dts'),
         },
       });
     }
