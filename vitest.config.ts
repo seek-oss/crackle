90ef1ea05cb6fcc440269df9ff3da339ee843c4e
Remus Mate <rmate@seek.com.au>
2022-12-22 10:31:34 +1100

    fix linting errors

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
 
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
