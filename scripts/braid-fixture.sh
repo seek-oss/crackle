3662305be9e1cb3c88261e6adc756485247e5a27
Remus Mate <rmate@seek.com.au>
2023-02-09 17:20:20 +1100

    update Braid submodule to v32

--- scripts/braid-fixture.sh
+++ scripts/braid-fixture.sh
@@ -3,7 +3,7 @@ set -eox pipefail
 
 repo=git@github.com:seek-oss/braid-design-system.git
 submodule=fixtures/braid-design-system
-branch=compiled-braid
+branch=v32
 
 # Modified from https://stackoverflow.com/questions/45688121/how-to-do-submodule-sparse-checkout-with-git/45689692#45689692
 
