78367fcbfb7deb7086078fd23330892b2bf281a6
Remus Mate <rmate@seek.com.au>
2023-02-13 12:32:01 +1100

    consolidate fixtures with patched imports

--- fixtures/esm-patch-imports/src/index.ts
+++ fixtures/esm-patch-imports/src/index.ts
@@ -1,4 +1,6 @@
-export { default as subpath } from '@crackle-fixtures/dep-with-exports/subpath';
-export { default as patchedSubpath } from '@crackle-fixtures/dep-with-exports-always-patch/subpath';
+export { default as scopedSubpath } from '@crackle-fixtures/dep-with-exports/subpath';
+export { default as scopedPatchedSubpath } from '@crackle-fixtures/dep-with-exports-always-patch/subpath';
+
+export { default as cjsDirIndex } from 'autosuggest-highlight/parse';
 
 export { App, render } from './render';
