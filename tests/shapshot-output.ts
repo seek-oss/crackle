7d246f601908afe88632e00c44de7cde0dfd8e2e
Remus Mate <rmate@seek.com.au>
2023-04-11 16:34:56 +1000

    replace `jest-specific-snapshot` with builtin `.toMatchFileSnapshot` from Vitest 0.30.0

--- tests/shapshot-output.ts
+++ tests/shapshot-output.ts
@@ -4,11 +4,9 @@ import path from 'path';
 import glob from 'fast-glob';
 import fixturez from 'fixturez';
 import _ from 'lodash';
-import { addSerializer as addSpecificSnapshotSerializer } from '~utils/specific-snapshot-serializer';
+import { expect } from 'vitest';
 
 const f = fixturez(__dirname);
-
-addSpecificSnapshotSerializer();
 
 const promiseMap = async <T, K>(
   items: T[],
@@ -31,19 +29,20 @@ export default async function snapshotOutput(
   );
 
   await promiseMap(Object.entries(groups), async ([groupName, files]) => {
-    await Promise.all(
-      files.map(async (fileName) => {
-        const contents = await fs.readFile(
-          path.join(fixtureDir, fileName),
-          'utf-8',
-        );
-        expect(contents).toMatchSpecificSnapshot(
-          `__snapshots__/${suiteName}/${fixtureName}/${groupName}.snap`,
-          // @ts-ignore Types don't match the implementation
-          // https://github.com/igor-dv/jest-specific-snapshot/blob/v8.0.0/src/index.js#L53
-          fileName,
-        );
-      }),
+    const bundle = await promiseMap(files, async (fileName) => {
+      const contents = await fs.readFile(
+        path.join(fixtureDir, fileName),
+        'utf-8',
+      );
+      return `/* #region ${fileName} */\n${contents}/* #endregion */\n`;
+    });
+
+    const snapshotDir = `__snapshots__/${suiteName}/${fixtureName}`;
+    await fs.mkdir(`${__dirname}/${snapshotDir}/${path.dirname(groupName)}`, {
+      recursive: true,
+    });
+    await expect(bundle.join('\n\n')).toMatchFileSnapshot(
+      `${snapshotDir}/${groupName}.ts.snap`,
     );
   });
 }
