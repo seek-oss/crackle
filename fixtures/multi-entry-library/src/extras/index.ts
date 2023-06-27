3b388d2098315d9379563646397d91060d5f01ef
Remus Mate <rmate@seek.com.au>
2023-06-27 11:33:12 +1000

    Ensure esbuild can run on `crackle dev` entry points (#123)

--- fixtures/multi-entry-library/src/extras/index.ts
+++ fixtures/multi-entry-library/src/extras/index.ts
@@ -1,13 +1,12 @@
-// @crackle/core is a devDependency so the types should be inlined
-import type { logger as crackleLogger } from '@crackle/core/logger';
+// this is a devDependency so the types will be inlined
+import devDep from '@crackle-fixtures/dev-dep';
+export type { DevDepType } from '@crackle-fixtures/dev-dep';
 
-import { logThePhrase } from '../common/commonLogger';
 import type { MathsFn } from '../common/maths';
 
-export type InlinedType = typeof crackleLogger;
+export const logger = (...args: any[]) =>
+  // eslint-disable-next-line no-console
+  console.log('This logs', devDep, ':', ...args);
 
-export const logger = () => logThePhrase('This is a logger');
-
-export const calcAndLog = (a: number, b: number, fn: MathsFn) => {
-  logThePhrase(fn(a, b).toString());
-};
+export const calcAndLog = (a: number, b: number, fn: MathsFn) =>
+  logger(fn(a, b).toString());
