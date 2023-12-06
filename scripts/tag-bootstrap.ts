e8738f2dd0550fe553d8665445e00ed0bf107dc1
Remus Mate <rmate@seek.com.au>
2023-12-06 11:37:31 +1100

    add some more comments

--- scripts/tag-bootstrap.ts
+++ scripts/tag-bootstrap.ts
@@ -1,5 +1,8 @@
 import { run, done } from './utils';
 
+// We tag the latest version because this is meant to run on `prepublishOnly` (which means we have a new version be published to npm, so bootstrap will be the latest-1).
+// That way we can install the latest from npm without pnpm linking to the workspace package, which is not desired.
+
 const latest = `${await run(`npm info @crackle/cli@latest version`, {
   encoding: 'utf-8',
   stdio: 'pipe',
