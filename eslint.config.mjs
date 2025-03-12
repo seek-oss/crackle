ba2f630f33736548076ea3732e2f5c8b45c6d35a
Adam Skoufis <askoufis@seek.com.au>
2025-03-12 14:42:33 +1100

    Implement `ignore-sync` functionality that works with eslint flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,64 +1,43 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { includeIgnoreFile } from '@eslint/compat';
 import eslintConfigSeek from 'eslint-config-seek';
+import glob from 'fast-glob';
 
 import formatJsTag from '@crackle/eslint-plugin-format-js-tag';
+
+// Recreate the behaviour of `ignore-sync`, but make it work for eslint flat config.
+// TL;DR: Get all `.gitignore` entries in packages and fixtures and make them relative
+// to the repo root.
+const gitignorePaths = [
+  ...(await glob.async('(fixtures|packages)/*/.gitignore')),
+];
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const packageAndFixtureIgnoresEntries = [];
+
+for (const gitignorePath of gitignorePaths) {
+  const resolvedPath = path.resolve(__dirname, gitignorePath);
+  const ignoreConfig = includeIgnoreFile(resolvedPath);
+
+  const dir = path.dirname(gitignorePath);
+
+  ignoreConfig.ignores = ignoreConfig.ignores.map((ignore) =>
+    path.join(dir, ignore),
+  );
+
+  packageAndFixtureIgnoresEntries.push(ignoreConfig);
+}
 
 /** @type {import("eslint").Linter[]} */
 export default [
   {
-    ignores: [
-      // Inline
-      'fixtures/braid-design-system',
-      '**/node_modules',
-
-      // Ignore entries in fixture gitignores
-      'fixtures/braid-site/dist',
-      'fixtures/dev-entries/cli',
-      'fixtures/dev-entries/deep/re-export',
-      'fixtures/dev-entries/dist',
-      'fixtures/dev-entries-webpack/cli',
-      'fixtures/dev-entries-webpack/dist',
-      'fixtures/dts-preserve/dist',
-      'fixtures/esm-compat/dist',
-      'fixtures/library-with-docs/dist',
-      'fixtures/multi-entry-library/components',
-      'fixtures/multi-entry-library/dist',
-      'fixtures/multi-entry-library/extras',
-      'fixtures/multi-entry-library/themes/apac',
-      'fixtures/package-preserve/dist',
-      'fixtures/package-preserve-dts-preserve/dist',
-      'fixtures/single-entry-library/dist',
-      'fixtures/with-dep-hidden-package-json/dist',
-      'fixtures/with-graphql-schema-types/dist',
-      'fixtures/with-side-effects/css',
-      'fixtures/with-side-effects/css-more',
-      'fixtures/with-side-effects/dist',
-      'fixtures/with-side-effects/reset',
-      'fixtures/with-styles/dist',
-      'fixtures/with-vocab/dist',
-      'fixtures/with-vocab/**/*.vocab/index.ts',
-
-      // Ignore entries in fixture gitignores
-      'packages/babel-plugin-remove-exports/dist',
-      'packages/cli/config',
-      'packages/cli/dist',
-      'packages/cli/**/README.md',
-      'packages/core/build',
-      'packages/core/clean',
-      'packages/core/dev',
-      'packages/core/dist',
-      'packages/core/fix',
-      'packages/core/logger',
-      'packages/core/package',
-      'packages/core/resolve-config',
-      'packages/core/route-data',
-      'packages/core/serve',
-      'packages/core/start',
-      'packages/eslint-plugin-format-js-tag/dist',
-      'packages/router/dist',
-      'packages/router/routes',
-      'packages/router/server',
-    ],
+    ignores: ['fixtures/braid-design-system', '**/node_modules'],
   },
+  ...packageAndFixtureIgnoresEntries,
   {
     files: ['packages/babel-plugin-remove-exports/src/index.test.ts'],
     plugins: {
